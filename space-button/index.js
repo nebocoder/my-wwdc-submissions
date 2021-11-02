7f25e1a8483c63cce9621d5bd9bc518a8e2a19ac
Nemanja <91620216+nebocoder@users.noreply.github.com>
2021-11-02 12:44:37 +0100

    Add switch theme option

--- space-button/index.js
+++ space-button/index.js
@@ -1,1 +1,20 @@
 const exploreButton = document.getElementById("explore-btn")
+const body = document.querySelector("body")
+
+exploreButton.addEventListener("click", themeSwitch)
+
+function themeSwitch() {
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark")
+    body.classList.add("light")
+
+    exploreButton.classList.remove("btn-light")
+    exploreButton.classList.add("btn-dark")
+  } else {
+    body.classList.remove("light")
+    body.classList.add("dark")
+
+    exploreButton.classList.remove("btn-dark")
+    exploreButton.classList.add("btn-light")
+  }
+}
